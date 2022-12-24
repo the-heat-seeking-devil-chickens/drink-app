fa0cbf374ec50c6d8fd4812c4ba3143336e2a2f4
wmoody6293 <wmoody6293@gmail.com>
2022-12-23 20:40:52 -0500

    updated backend template

---
+++ backend/server/server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const Router = require('./routes/api');
+const PORT = 8000;
+
+/**
+ * handle parsing request body
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * handle requests for static files
+ */
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+/**
+ * define route handlers
+ */
+app.use('/api', apiRouter);
+
+/**
+ * express error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
